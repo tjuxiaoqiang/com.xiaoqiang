d1fdde6cfaf5eac659a38130c6cce8d3e379013a
tjuxiaoqiang <tjuxiaoqiang@163.com>
2014-09-07 13:07:11 +0800

    init commit

--- kafka-monitor.sh
+++ kafka-monitor.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-java -Xms512M -Xmx512M -Xss1024K -XX:PermSize=256m -XX:MaxPermSize=512m -cp /home/xiaoqiang/workspace/KafkaOffsetMonitor-0.2.0/target/scala-2.10/KafkaOffsetMonitor-assembly-0.1.0-SNAPSHOT.jar \
+java -jar ./kafka-proxy-0.0.1-SNAPSHOT-jar-with-dependencies.jar &
+
+java -Xms512M -Xmx512M -Xss1024K -XX:PermSize=256m -XX:MaxPermSize=512m -cp ./KafkaOffsetMonitor-assembly-0.1.0-SNAPSHOT.jar \
      com.quantifind.kafka.offsetapp.OffsetGetterWeb --zk localhost:2181 \
      --port 8086 \
      --refresh 10.seconds \
